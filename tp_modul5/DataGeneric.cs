b6357d10fc53850c983580dd98766d715c47a25c
rengganistan <rengganistantri4@gmail.com>
2025-03-11 08:57:24 +0700

    Menambahkan class DataGeneric

---
+++ tp_modul5/DataGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_modul5
+{
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+
+}
--- tp_modul5/Program.cs
+++ tp_modul5/Program.cs
@@ -1,8 +1,15 @@
-﻿class Program
+﻿using tp_modul5;
+
+class Program
 {
     static void Main()
     {
+        // Memanggil HaloGeneric
         HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser("Rengganis"); 
+        halo.SapaUser("Rengganis");
+
+        // Memanggil DataGeneric
+        DataGeneric<string> data = new DataGeneric<string>("2311104065");
+        data.PrintData();
     }
 }
